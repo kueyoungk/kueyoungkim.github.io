6dd57c298b5e1abde56ed4cd97a253569597d9a2
kueyoungkim@gmail.com <143348100+kueyoungk@users.noreply.github.com>
2024-12-15 09:33:59 -0500

    finish abstracts in projects section

--- script.js
+++ script.js
@@ -1,19 +1,16 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     const toggleButtons = document.querySelectorAll('.toggle-button');
 
-    toggleButtons.forEach(button => {
-        button.addEventListener('click', function() {
+    toggleButtons.forEach((button) => {
+        button.addEventListener('click', function () {
             const projectInfo = button.closest('.project-info');
             const moreText = projectInfo.querySelector('.more-text');
 
             if (moreText.style.display === 'none' || moreText.style.display === '') {
-                // Show the hidden text
-                moreText.style.display = 'inline';
+                moreText.style.display = 'block'; 
                 button.textContent = 'See Less';
             } else {
-                // Hide the text again
                 moreText.style.display = 'none';
-                button.textContent = 'See More';
             }
         });
     });
